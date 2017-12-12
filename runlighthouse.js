d45484c1634bb053f8d90477dd24bef09a1a909d
Jake LeBoeuf <dev@jakeleboeuf.com>
2017-12-12 15:02:28 -0500

    get repo things

--- runlighthouse.js
+++ runlighthouse.js
@@ -145,6 +145,8 @@ function getConfig() {
     name: repoSlug ? repoSlug.split('/')[1] : process.env.CIRCLE_PROJECT_REPONAME
   };
 
+  console.log(config.repo.owner, config.repo.name);
+
   return config;
 }
 
